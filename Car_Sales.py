3bf4b7099a27f274174e73b9514cb9af7f592beb
aliemirkoca <aliemirkoca@gmail.com>
2025-08-31 23:39:30 +0300

    Avarage_Price

--- Car_Sales.py
+++ Car_Sales.py
@@ -1,7 +1,9 @@
 import pandas as pd
-import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
 import zipfile
+import os 
+
 
 zip_path = r"C:\Users\aliem\Downloads\car_sales.zip"
 extract_path = r"C:\Users\aliem\Downloads\car_sales"
@@ -9,4 +11,15 @@ extract_path = r"C:\Users\aliem\Downloads\car_sales"
 with zipfile.ZipFile(zip_path, 'r') as zip_ref:
     zip_ref.extractall(extract_path)
 
-print("Dosyalar buraya açıldı:", extract_path)
+df = pd.read_csv(os.path.join(extract_path, "car_sales_data.csv"))
+
+print(df.head())
+
+avg_price=df.groupby("Manufacturer")["Price"].mean().sort_values(ascending=False)
+
+plt.figure(figsize=(10,6))
+avg_price.plot(kind="bar")
+plt.title("Average Price by Manufacturer")
+plt.ylabel("Average Price")
+plt.xlabel("Manufacturer")
+plt.show()
