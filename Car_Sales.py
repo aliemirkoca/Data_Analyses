f5cfd91f69c62654e844e50c916bd572243c9150
aliemirkoca <aliemirkoca@gmail.com>
2025-09-01 21:55:26 +0300

    price_to_km

--- Car_Sales.py
+++ Car_Sales.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import zipfile
 import os 
-
+import seaborn as sns
 
 zip_path = r"C:\Users\aliem\Downloads\car_sales.zip"
 extract_path = r"C:\Users\aliem\Downloads\car_sales"
@@ -12,14 +12,26 @@ with zipfile.ZipFile(zip_path, 'r') as zip_ref:
     zip_ref.extractall(extract_path)
 
 df = pd.read_csv(os.path.join(extract_path, "car_sales_data.csv"))
+df["KM"]=df["Mileage"]* 1.6
 
-print(df.head())
 
+print(df.head)
 avg_price=df.groupby("Manufacturer")["Price"].mean().sort_values(ascending=False)
+avg_km=df.groupby("Manufacturer")["KM"].mean().sort_values(ascending=False)
+
+
+#plt.figure(figsize=(10,6))
+#avg_km.plot(kind="line")
+#plt.title("Average KM by Manufacturer")
+#plt.ylabel("Average KM")
+#plt.xlabel("Manufacturer")
+#plt.show()
+
 
 plt.figure(figsize=(10,6))
-avg_price.plot(kind="bar")
-plt.title("Average Price by Manufacturer")
-plt.ylabel("Average Price")
-plt.xlabel("Manufacturer")
+sns.scatterplot(x="KM", y="Price", data=df, hue="Fuel type", palette="deep")
+plt.title("Mileage vs Price (colored by Fuel Type)")
+plt.xlabel("Kilometers")
+plt.ylabel("Price")
 plt.show()
+
